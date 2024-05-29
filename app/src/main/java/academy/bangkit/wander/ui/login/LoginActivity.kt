93292ebd204efdee55971ed2f5a350d44445df34
Nur Fajar Sayyidul Ayyam <nur.fajar11@ui.ac.id>
2024-05-29 19:04:31 +0700

    Initial commit

---
+++ app/src/main/java/academy/bangkit/wander/ui/login/LoginActivity.kt
@@ -0,0 +1,9 @@
+package academy.bangkit.wander.ui.login
+
+import academy.bangkit.wander.core.base.BaseActivity
+import academy.bangkit.wander.databinding.ActivityLoginBinding
+
+class LoginActivity : BaseActivity<ActivityLoginBinding>(ActivityLoginBinding::inflate) {
+    override fun ActivityLoginBinding.initialBinding() {
+    }
+}
