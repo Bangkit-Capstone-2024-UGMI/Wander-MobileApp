8be317a654e9f510bb52dee59a9155a105daf58a
Nur Fajar Sayyidul Ayyam <nur.fajar11@ui.ac.id>
2024-06-16 15:57:38 +0700

    fix: bottom nav bug

--- app/src/main/java/academy/bangkit/wander/presentation/main/MainScreen.kt
+++ app/src/main/java/academy/bangkit/wander/presentation/main/MainScreen.kt
@@ -26,6 +26,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 
@@ -63,7 +67,9 @@ fun MainScreen(navController: NavHostController, mainNavController: NavHostContr
                 routeName = AppRoute.ACCOUNT
             ),
         )
-        var selectedItemIndex = 0
+        var selectedItemIndex by rememberSaveable {
+            mutableIntStateOf(0)
+        }
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
